22f960fa7445509c9968b5470a9a17a5078a6ce0
Dreary Lisper <drearylisper@gmail.com>
2022-01-05 20:30:04 +0000

    Send broadcast requests

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,25 @@
 module Main where
 
+import Control.Monad
+import Control.Concurrent
+
+import Network.HostName
+import Network.Socket
+import Network.Socket.ByteString
+import qualified Data.ByteString.Char8 as C
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  addrinfos <- getAddrInfo Nothing (Just "0.0.0.0") (Just "7000")
+  let serveraddr = head addrinfos
+  sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
+  bind sock (addrAddress serveraddr)
+  setSocketOption sock ReuseAddr 1
+  setSocketOption sock Broadcast 1
+  print sock
+  forkIO $ forever (do recv sock 4096 >>= print)
+  forever $ do
+    remoteAddrs <- getAddrInfo Nothing (Just "255.255.255.255") (Just "7000")
+    hostname <- getHostName
+    sendTo sock (C.pack hostname) (addrAddress $ head remoteAddrs)
+    threadDelay 1000000
