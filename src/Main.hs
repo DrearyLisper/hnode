0e2b081727fa82fa85048d0ee9edb680eaad8d7c
Dreary Lisper <drearylisper@gmail.com>
2022-01-05 23:02:06 +0000

    Change message

--- src/Main.hs
+++ src/Main.hs
@@ -24,5 +24,5 @@ main = do
   forever $ do
     remoteAddrs <- getAddrInfo Nothing (Just "255.255.255.255") (Just "7000")
     hostname <- getHostName
-    sendTo sock (C.pack $ "Hello from " ++ hostname) (addrAddress $ head remoteAddrs)
+    sendTo sock (C.pack $ "Hello from " ++ hostname ++ "!") (addrAddress $ head remoteAddrs)
     threadDelay 1000000
